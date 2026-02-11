a8b64baa02e13e3804b8099718ba11aad6e3857f
Billy Alvear <alvearbilly@gmail.com>
2026-02-10 22:26:18 -0500

    create-spec-cv

---
+++ src/app/calculator/views/calculator-view/calculator-view.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import CalculatorView from './calculator-view';
+
+describe('CalculatorView', () => {
+  let component: CalculatorView;
+  let fixture: ComponentFixture<CalculatorView>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [CalculatorView],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CalculatorView);
+    component = fixture.componentInstance;
+    fixture.detectChanges(); // importante
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
